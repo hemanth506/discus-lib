f747d3f236991dcbe692a0efcf8e7c0d0add48ed
Hemanth Raaj G <hemanthraaj31@gmail.com>
2024-07-21 15:08:58 +0530

    Package publish code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import './App.css';
-import { CommentBox } from './components/Comment-Box/CommentBox';
-import { CommentThread } from './components/Comment-Thread/CommentThread';
+import React from "react";
+import "./App.css";
+import { CommentBox } from "./components/Comment-Box/CommentBox";
+import { CommentThread } from "./components/Comment-Thread/CommentThread";
+import { CommentProvider } from "./contexts/CommentContext";
 
 function App() {
   return (
-    <div className="App">
-      <CommentBox />
-      <CommentThread />
-    </div>
+    <CommentProvider>
+      <div className="App">
+        <CommentBox />
+        <CommentThread />
+      </div>
+    </CommentProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
-// import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { CommentProvider } from "./contexts/CommentContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   // <React.StrictMode>
-    <CommentProvider>
       <App />
-    </CommentProvider>
   // </React.StrictMode>
 );
