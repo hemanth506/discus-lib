4ec78008616ae3c8ebbe45d1bad46852f83665ea
Hemanth Raaj G <hemanthraaj31@gmail.com>
2024-07-21 15:31:45 +0530

    Removed App.css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 import { CommentBox } from "./components/Comment-Box/CommentBox";
 import { CommentThread } from "./components/Comment-Thread/CommentThread";
 import { CommentProvider } from "./contexts/CommentContext";
 
 function App() {
   return (
-    <CommentProvider>
-      <div className="App">
+    <div className="App">
+      <CommentProvider>
         <CommentBox />
         <CommentThread />
-      </div>
-    </CommentProvider>
+      </CommentProvider>
+    </div>
   );
 }
 
