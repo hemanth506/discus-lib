3a985b9baea332e7874703b2b3c9d78c97808933
Hemanth Raaj G <hemanthraaj31@gmail.com>
2024-07-21 15:37:20 +0530

    Removed commented line

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import "./App.css";
 import { CommentBox } from "./components/Comment-Box/CommentBox";
 import { CommentThread } from "./components/Comment-Thread/CommentThread";
 import { CommentProvider } from "./contexts/CommentContext";
