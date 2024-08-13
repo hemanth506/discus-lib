28f0cff79158de10fe68a35634afab8020c05017
Hemanth Raaj G <hemanthraaj31@gmail.com>
2024-08-13 14:54:11 +0530

    Function changes.

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -62,28 +62,27 @@ export const Comment: React.FC<CommentCompType> = ({ cmt, setComments }) => {
     let interval = Math.floor(seconds / 31536000);
 
     if (interval >= 1) {
-      return interval + interval === 1 ? " year" : " years";
+      return interval + (interval === 1 ? " year" : " years");
     }
     interval = Math.floor(seconds / 2592000);
     if (interval >= 1) {
-      return interval + interval === 1 ? " month" : " months";
+      return interval + (interval === 1 ? " month" : " months");
     }
     interval = Math.floor(seconds / 86400);
     if (interval >= 1) {
-      return interval + interval === 1 ? " day" : " days";
+      return interval + (interval === 1 ? " day" : " days");
     }
     interval = Math.floor(seconds / 3600);
     if (interval >= 1) {
-      return interval + interval === 1 ? " hour" : " hours";
+      return interval + (interval === 1 ? " hour" : " hours");
     }
     interval = Math.floor(seconds / 60);
     if (interval >= 1) {
-      return interval + interval === 1 ? " minute" : " minutes";
+      return interval + (interval === 1 ? " minute" : " minutes");
     }
     if (Math.floor(seconds) !== 0) {
       return Math.floor(seconds) + " seconds";
     }
-
     return "Just now";
   };
 
